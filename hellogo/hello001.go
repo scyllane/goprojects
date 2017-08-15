e4d43c21b2f473c63f6a1590c80c8bfd298d65a9
scyllane <scyllane@hotmail.com>
2017-08-15 20:19:40 +0800

    update

---
+++ hellogo/hello001.go
@@ -0,0 +1,5 @@
+package hello
+import "fmt"
+func Hello001() {
+fmt.Println("This is Hello 001. ")
+}
---
+++ hellogo/hello002.go
@@ -0,0 +1,5 @@
+package hello
+import "fmt"
+func Hello002() {
+fmt.Println("This is Hello 002. ")
+}
